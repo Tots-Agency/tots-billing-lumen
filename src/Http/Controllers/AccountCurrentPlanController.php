7f67ff3c694071caff48cb05ab1d46c794604d72
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-10 21:53:08 -0300

    Add current plan by account controller.

---
+++ src/Http/Controllers/AccountCurrentPlanController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tots\Billing\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tots\Billing\Models\TotsAccountPlan;
+
+class AccountCurrentPlanController extends \Laravel\Lumen\Routing\Controller
+{
+    public function handle($accountId, Request $request)
+    {
+        /** @var \Tots\Account\Models\TotsAccount $account */
+        $account = $request->input(TotsAccount::class);
+
+        $accPlan = TotsAccountPlan::where('account_id', $account->id)->orderBy('id', 'desc')->first();
+        if($accPlan == null){
+            throw new \Exception('The account not has plan');
+        }
+
+        return $accPlan;
+    }
+}
